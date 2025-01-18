c93697005c7c700b8477efbf0a2b84b9b7da166e
ziane badr eddine <zianebadredddine2004@gmail.com>
2025-01-18 17:20:34 +0100

    test_

---
+++ app/user/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = ({ params }: { params: { userId: string }} ) => {
+  return (
+    <div>
+        {params.userId}
+    </div>
+  )
+}
+
+export default page
